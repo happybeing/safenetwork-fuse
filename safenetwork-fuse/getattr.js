56309d09a6eba1773a92c7eb683588901840e97e
theWebalyst <github@happybeing.com>
2018-06-12 21:30:16 +0100

    Simulate directories and files for PoC build

--- safenetwork-fuse/getattr.js
+++ safenetwork-fuse/getattr.js
@@ -2,6 +2,27 @@ const Fuse = require('fuse-bindings')
 const explain = require('explain-error')
 const debug = require('debug')('ipfs-fuse:getattr')
 
+// TODO remove this:
+
+const isFolder = function (path) {
+  return path.substr(-1) === '/'
+}
+
+const fakeReadDir = {
+  '/': ['one', 'two', 'three'],
+  '/one': ['four', 'five', 'happybeing']
+}
+
+const fakeGetattr = {
+  '/': 'directory',
+  '/one': 'directory',
+  '/two': 'file',
+  '/three': 'file',
+  '/one/four': 'file',
+  '/one/five': 'file',
+  '/one/happybeing': 'file'
+}
+
 module.exports = (ipfs) => {
   const now = Date.now()
 
@@ -9,7 +30,27 @@ module.exports = (ipfs) => {
     getattr (path, reply) {
       debug({ path })
 
+      result = fakeGetattr[path]
+
+if (result) {
+      // TODO stop FAKING IT:
+      reply(0, {
+        mtime: now,
+        atime: now,
+        ctime: now,
+        nlink: 1,
+        size: 1234,
+        // https://github.com/TooTallNate/stat-mode/blob/master/index.js
+        mode: result == 'directory' ? 16877 : 33188,
+        uid: process.getuid ? process.getuid() : 0,
+        gid: process.getgid ? process.getgid() : 0
+      })
+} else {
+  reply(Fuse.ENOENT)
+}
+if (false){
       ipfs.files.stat(path, (err, stat) => {
+
         if (err) {
           if (err.message === 'file does not exist') return reply(Fuse.ENOENT)
           err = explain(err, 'Failed to stat path')
@@ -29,6 +70,7 @@ module.exports = (ipfs) => {
           gid: process.getgid ? process.getgid() : 0
         })
       })
+}
     }
   }
 }
--- safenetwork-fuse/readdir.js
+++ safenetwork-fuse/readdir.js
@@ -2,11 +2,33 @@ const Fuse = require('fuse-bindings')
 const explain = require('explain-error')
 const debug = require('debug')('ipfs-fuse:readdir')
 
+const fakeReadDir = {
+  '/': ['one', 'two', 'three'],
+  '/one': ['four', 'five', 'happybeing']
+}
+
+const fakeGetattr = {
+  '/': 'directory',
+  '/one': 'directory',
+  '/two': 'file',
+  '/three': 'file',
+  '/one/four': 'file',
+  '/one/five': 'file',
+  '/one/happybeing': 'file'
+}
+
 module.exports = (ipfs) => {
   return {
     readdir (path, reply) {
       debug({ path })
 
+      let listing = fakeReadDir[path]
+      if (listing)
+        reply(0, listing)
+      else
+        reply(Fuse.EREMOTEIO)
+
+      /*
       ipfs.files.ls(path, (err, files) => {
         if (err) {
           err = explain(err, 'Failed to ls path')
@@ -14,7 +36,7 @@ module.exports = (ipfs) => {
           return reply(Fuse.EREMOTEIO)
         }
         reply(0, files.map(f => f.name || f.hash))
-      })
+      })*/
     }
   }
 }
