6348723b8aee87582893c08882aaa3d7d2059be2
theWebalyst <github@happybeing.com>
2018-07-24 16:30:27 +0100

    Update SafeVfs and vfsHandler 'DESIGN' in safe-vfs

--- src/safe-vfs/index.js
+++ src/safe-vfs/index.js
@@ -1,9 +1,10 @@
 /* TODO theWebalyst notes:
-[ ] add support for CLI to configure mounting:
+[ ] Implement SafeVfs class and vfsHandler classes according to 'DESIGN' below
+[ ] LATER add support for CLI to configure mounting:
     SafeVfs currently hard codes a default set of path mappings, this should
     be replaced by settings from the CLI parameters, so users can choose what
     to mount and where.
-[ ] Async: looks like I could replace with Promises (https://caolan.github.io/async/docs.html#auto)
+[ ] LATER Async: looks like I could replace with Promises (https://caolan.github.io/async/docs.html#auto)
   -> tried but didn't work so leave for later.
 
 SAFE-VFS - DESIGN (July 2018)
@@ -19,10 +20,35 @@ SafeVfs
 -------
 SafeVfs implements the Path Map containing entries which map a path
 to a vfsHandler object. The Path Map contains:
-- one top level entry for each mount point (e.g. _publicNames, _documents etc.)
-- zero or more entries for sub-paths of a top level path entry, that are
-  also containers
+- an entry for '/' with an instance of vfsRootHandler
+- a top level container entry for each mount point (e.g. _publicNames, _documents etc.)
+- zero or sub-path entries that are also containers
 - provides a getHandler() to get the vfsHandler for a given path (see next)
+
+FUSE Operations
+---------------
+Each supported FUSE operation (such as readdir, statfs etc) is implemented in
+its own file in ../fuse-operations.
+
+When called, the FUSE operation calls SafeVfs.getHandler() to get a vfsHandler
+object from the map, corresponding to the target (path), or an error if this
+fails. It then checks that its corresponding FUSE operation is implemented on
+the handler. So if this is FUSE operation readdir() it checks for a readdir()
+method on the vfsHandler object and calls it. If the method isn't present,
+it returns an error.
+
+SafeVfs.newHandler({key, lazyInitialise, {params} })
+----------------------------------------------------
+SafeVfs.newHandler() returns a new vfsHandler object (see below)
+corresponding to the role of the value at the given key (typically a Mutable
+Data object).
+
+The returned handler object will cache any supplied 'params' (such as a key
+within the container which they handle).
+
+The 'lazyInitialise' flag determines whether to initialise immediately (to
+access a Mutable Data for example), or to return immediately and delay
+initialisation until needed.
 
 SafeVfs.getHandler()
 --------------------
@@ -31,54 +57,35 @@ exists, it returns the entry, a vfsHandler object. If there is no entry
 matching the path getHandler() recurses by calling itself on the parent path
 and on resolving inserts the returned vfsHandler object into the map.
 
-vfsHandlerObject
-----------------
-vfsHandlerObject = {
-  fuseHandler <vfsFuseHandler>, // Handler for MD role at the leaf of path
-                                // (e.g public names, services, NFS service etc
+vfsHandler Object
+-----------------
+A vfsHandler object is an instance of a class such as vfsPublicNamesHandler or
+vfsNfsHandler. Those classes are each implemented in their own file in
+src/safe-vfs, and are required() for use in src/safe-vfs/index.js
 
-  container <mutableData>,  // The corresponding MD (null if lazy init)
-  params <object>           // Params (e.g. as a key in the container if the
-                            // mounted path is below the container root)
+A handler object implements a FUSE operation method for each FUSE operations it
+supports such as readdir(), statfs() etc. (see src/fuse-operations)
+
+Each handler class has a constructor which takes params (eg safePath, mountPath,
+lazyInit) and will probably cache API related information to speed access
+to the MutableData which stores its content.
+
+When a handler lists a container, it uses the list of items it contains to
+update the pathMap (deleting or adding entries to bring it up to date) in case
+any of its contents have been changed. Some (all?) handlers might update that
+list for some (all?) other operations.
+
+For example inside index.js we will have:
+
+vfsPublicNames=require('vfsPublicNames')
+
+// and later when adding to the PathMap:
+
+let handler = new vfsPublicNames(safeApi, '_publicNames')
+if (handler) {
+  pathMap.'_publicNames' = handler
 }
 
-vfsHandler.fuseHandler()
-------------------------
-This method is called by the FUSE operator implementations. Below is
-example SAFE-VFS code for the FUSE readdir operator (implemented in readdir.js):
-
-  module.exports = (safeVfs) => {
-    return {
-      readdir (path, cb) {
-        debug({ path })
-        try {
-          cb(0, await safeVfs.fuseHandler(path).readdir(path))
-          catch (err) {
-            err = explain(err, 'Failed to readdir path: ' + path)
-            debug(err)
-            cb(Fuse.EREMOTEIO)
-          }
-        })
-      }
-    }
-  }
-
-vfsFuseHandler classes
-----------------------
-A vfsHandlerObject is an instance of a vfsFuseHandler<type> class, where there
-is a type corresponds to the MD container role: PublicNames, Services,
-ServiceNfs, ServiceEmail
-
-Each class provides a method for each supported FUSE operation (readdir,
-mkdir etc).
-
-newVfsFuseHandler({key, lazyInitialise, {params} })
-Another method, newVfsFuseHandler(key) returns a new vfsHandlerObject
-corresponding to the MD role of the value at the given key. The new
-object will contain any supplied 'params' (such as a key within the
-container which they handle), and a 'lazyInitialise' flag which determines
-whether to initialise the 'container' member using the API, or set it to
-null and return immediately. See vfsHandlerObject above.
 */
 
 const Fuse = require('fuse-bindings')
