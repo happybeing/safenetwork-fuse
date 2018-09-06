f80bd3a9cbfab697a439027a4fc7049f11d60b06
theWebalyst <github@happybeing.com>
2018-09-06 16:14:36 +0100

    Make open() return dummy file descriptor

--- src/fuse-operations/open.js
+++ src/fuse-operations/open.js
@@ -10,7 +10,10 @@ module.exports = (safeVfs) => {
 
         safeVfs.getHandler(itemPath).open(itemPath, flags).then((file) => {
           debug('open success')
-          reply(0, file)  // TODO does file work??? 42 is a file descriptor (https://github.com/mafintosh/fuse-bindings#opsopenpath-flags-cb)
+          // (https://github.com/mafintosh/fuse-bindings#opsopenpath-flags-cb)
+          // IPFS FUSE returned a file descriptor value of 42 which I guess is a joke
+          let fd = 1234 // TODO Consider using file descriptors in SafenetworkJs (see TODOs there)
+          reply(0, fd)
         })
       } catch (err) {
         let e = explain(err, 'Failed to open: ' + itemPath)
