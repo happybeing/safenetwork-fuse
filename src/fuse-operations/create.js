33adc37825167fc5330ccd01b6653754c6da863c
theWebalyst <github@happybeing.com>
2018-10-01 17:44:41 +0100

    Implement fuse create() and write()

--- src/fuse-operations/create.js
+++ src/fuse-operations/create.js
@@ -2,18 +2,32 @@ const Fuse = require('fuse-bindings')
 const explain = require('explain-error')
 const debug = require('debug')('safe-fuse:ops')
 
-module.exports = (ipfs) => {
+module.exports = (safeVfs) => {
   return {
-    write (itemPath, mode, reply) {
-      debug('create(\'%s\', %s)', itemPath, mode)
-      ipfs.files.write(itemPath, Buffer.from(''), { create: true }, (err) => {
-        if (err) {
-          err = explain(err, 'Failed to create file')
-          debug(err)
-          return reply(Fuse.EREMOTEIO)
-        }
-        reply(0)
-      })
+    create (itemPath, mode, reply) {
+      try {
+        debug('create(\'%s\', %s)', itemPath, mode)
+
+        safeVfs.getHandler(itemPath).create(itemPath, mode).then((fd) => {
+          debug('created file (%s): ', fd, itemPath)
+          reply(fd)
+        })
+      } catch (err) {
+        let e = explain(err, 'Failed to create file: ' + itemPath)
+        debug(e)
+        reply(Fuse.EREMOTEIO)
+      }
+
+    // write (itemPath, mode, reply) {
+    //   debug('create(\'%s\', %s)', itemPath, mode)
+    //   ipfs.files.write(itemPath, Buffer.from(''), { create: true }, (err) => {
+    //     if (err) {
+    //       err = explain(err, 'Failed to create file')
+    //       debug(err)
+    //       return reply(Fuse.EREMOTEIO)
+    //     }
+    //     reply(0)
+    //   })
     }
   }
 }
--- src/fuse-operations/write.js
+++ src/fuse-operations/write.js
@@ -4,17 +4,31 @@ const debug = require('debug')('safe-fuse:ops')
 
 module.exports = (ipfs) => {
   return {
-    write (itemPath, fd, buf, len, pos, reply) {
-      debug('write(\'%s\', %s, buf, %n, %n)', itemPath, fd, len, pos)
+      try {
+        debug('write(\'%s\', %s, buf, %s, %s)', itemPath, fd, len, pos)
 
-      ipfs.files.write(itemPath, buf, { offset: pos, count: len }, (err) => {
-        if (err) {
-          err = explain(err, 'Failed to write to file')
-          debug(err)
-          return reply(Fuse.EREMOTEIO)
-        }
-        reply(len)
-      })
-    }
+        safeVfs.getHandler(itemPath).write(itemPath, fd, buf, len, pos).then((bytes) => {
+          debug('wrote %s bytes', bytes)
+          debug('data: %s', buf.slice(0, bytes))
+          reply(bytes)
+        })
+      } catch (err) {
+        let e = explain(err, 'Failed to write file: ' + itemPath)
+        debug(e)
+        reply(Fuse.EREMOTEIO)
+      }
+
+  //   write (itemPath, fd, buf, len, pos, reply) {
+  //     debug('write(\'%s\', %s, buf, %n, %n)', itemPath, fd, len, pos)
+  //
+  //     ipfs.files.write(itemPath, buf, { offset: pos, count: len }, (err) => {
+  //       if (err) {
+  //         err = explain(err, 'Failed to write to file')
+  //         debug(err)
+  //         return reply(Fuse.EREMOTEIO)
+  //       }
+  //       reply(len)
+  //     })
+  //   }
   }
 }
