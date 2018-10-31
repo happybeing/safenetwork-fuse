9dab8a9c227c04db41df57c54f2fdd917c0a298c
theWebalyst <github@happybeing.com>
2018-10-31 21:00:25 +0000

    Fix rmdir() to correctly handle success

--- src/fuse-operations/rmdir.js
+++ src/fuse-operations/rmdir.js
@@ -20,22 +20,3 @@ module.exports = (safeVfs) => {
     }
   }
 }
-
-// module.exports = (ipfs) => {
-//   return {
-//     rmdir (itemPath, reply) {
-//       debug('TODO: implement fuse operation: rmdir'); return reply(Fuse.EREMOTEIO)
-//
-//       debug('rmdir(\'%s\')', itemPath)
-//
-//       ipfs.files.rm(itemPath, { recursive: true }, (err) => {
-//         if (err) {
-//           err = explain(err, 'Failed to delete directory')
-//           debug(err)
-//           return reply(Fuse.EREMOTEIO)
-//         }
-//         reply(0)
-//       })
-//     }
-//   }
-// }
