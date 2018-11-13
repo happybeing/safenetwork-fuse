b7bf2238c857a8d757a16ceafdb85a3c6234bde3
theWebalyst <github@happybeing.com>
2018-11-13 16:00:31 +0000

    Added debug statements

--- src/fuse-operations/readdir.js
+++ src/fuse-operations/readdir.js
@@ -10,12 +10,14 @@ module.exports = (safeVfs) => {
         if (fuseResult) {
           // Add any virtual directories to the itemPath directory's listing
           fuseResult.returnObject = safeVfs.vfsCache().mergeVirtualDirs(itemPath, fuseResult.returnObject)
+          debug('virtual directory result: %o', fuseResult.returnObject)
           return reply(fuseResult.returnCode, fuseResult.returnObject)
         }
 
         safeVfs.getHandler(itemPath).readdir(itemPath).then((result) => {
           // Add any virtual directories to the itemPath directory's listing
           result = safeVfs.vfsCache().mergeVirtualDirs(itemPath, result)
+          debug('directory result: %o', result)
           return reply(0, result)
         }).catch((e) => { throw e })
       } catch (err) {
